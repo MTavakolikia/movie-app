14099cd58955f0f9cb6f785f6c87f9670475d67d
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2025-01-07 21:18:22 +0330

    fix bug (movie type)

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,7 +1,7 @@
-import { Movie } from "@/types/moveTypes";
 import MovieCard from "./MovieCard";
+import { PopularMovie } from "@/types/popularMoviesTypes";
 
-export default function Results({ results }: { results: Movie[] }) {
+export default function Results({ results }: { results: PopularMovie[] }) {
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-3">
